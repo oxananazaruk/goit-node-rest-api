f7b2655d0be398baf487f623836f1d37ce108f9d
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-02-02 16:37:20 +0200

    filter by query added

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -3,12 +3,20 @@ const Contact = require("../models/contact");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "email");
+
+  const favoriteQuery = favorite !== undefined ? { favorite } : {};
+
+  const result = await Contact.find(
+    { owner, ...favoriteQuery },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "email");
+
   res.json(result);
 };
 
