92d253419436697effc7c2d535fee013aa8fb99e
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-02-09 20:54:23 +0200

    Update resizeFile.js

--- helpers/resizeFile.js
+++ helpers/resizeFile.js
@@ -1,5 +1,5 @@
 const Jimp = require("jimp");
-const { HttpError } = require("./HttpError");
+const HttpError = require("./HttpError");
 
 const resizeFile = (filePath, newPath) => {
   Jimp.read(filePath)
@@ -10,7 +10,7 @@ const resizeFile = (filePath, newPath) => {
       return newImage.write(newPath);
     })
     .catch((err) => {
-      console.error(err);
+      //   console.error(err);
       throw HttpError(500);
     });
 };
