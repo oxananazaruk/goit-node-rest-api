f1ce3375061599c5ec2687d52f06036929209d89
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2024-02-10 17:38:24 +0200

    fixed resize function

--- helpers/resizeFile.js
+++ helpers/resizeFile.js
@@ -1,19 +1,6 @@
 const Jimp = require("jimp");
 const HttpError = require("./HttpError");
 
-// const resizeFile = (filePath, newPath) => {
-//   Jimp.read(filePath)
-//     .then((image) => {
-//       return image.resize(250, 250);
-//     })
-//     .then((newImage) => {
-//       return newImage.write(newPath);
-//     })
-//     .catch((err) => {
-//       throw HttpError(500);
-//     });
-// };
-
 const resizeFile = async (filePath, newPath) => {
   try {
     const avatar = await Jimp.read(filePath);
